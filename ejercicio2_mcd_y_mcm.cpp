693efbdaf3a8e9dfadc9273ee3a87c19d0a0d1ae
yngomeza <yngomeza@unjbg.edu.pe>
2025-05-29 23:54:08 -0500

    Actualiza ejercicio2: Agrega validación de entrada

--- ejercicio2_mcd_y_mcm.cpp
+++ ejercicio2_mcd_y_mcm.cpp
@@ -21,6 +21,20 @@ int main() {
     cin >> num1;
     cout << "Ingresa el segundo numero positivo: ";
     cin >> num2;
+    
+      while (true) {
+        cout << "Ingresa el primer numero positivo: ";
+        cin >> num1;
+        if (num1 > 0) break;
+        cout << "Error: el numero debe ser positivo." << endl;
+    }
+
+    while (true) {
+        cout << "Ingresa el segundo numero positivo: ";
+        cin >> num2;
+        if (num2 > 0) break;
+        cout << "Error: el numero debe ser positivo." << endl;
+    }
 
     cout << "MCD(" << num1 << ", " << num2 << ") = " << mcd(num1, num2) << endl;
     cout << "MCM(" << num1 << ", " << num2 << ") = " << mcm(num1, num2) << endl;
