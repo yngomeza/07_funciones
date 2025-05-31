7fba85e7398827edd2b6bc1740b77615be0089c0
yngomeza <yngomeza@unjbg.edu.pe>
2025-05-30 20:03:15 -0500

    Actualizar ejercicio3: Agregar funcion resta de términos

--- ejercicio3_fibonacci.cpp
+++ ejercicio3_fibonacci.cpp
@@ -18,6 +18,23 @@ int Fibonacci(int cantidad) {
     return suma;
 }
 
+int RestaFibonacci(int cantidad) {
+    int a = 0, b = 1, siguiente, resta = 0;
+
+    for (int i = 0; i < cantidad; ++i) {
+        if (i == 0)
+            resta = a;
+        else
+            resta = resta - a;
+
+        siguiente = a + b;
+        a = b;
+        b = siguiente;
+    }
+
+    return resta;
+}
+
 int main() {
     int cantidad;
     
@@ -30,7 +47,10 @@ int main() {
     }
 
     int suma = Fibonacci(cantidad);
+    int resta = RestaFibonacci(cantidad);
+    
     cout << "Suma de los terminos: " << suma << endl;
-
+	cout << "Resta de los terminos: " << resta << endl;
+	
     return 0;
 }
