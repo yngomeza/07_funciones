9a2770a8473d6b7033b3a96f9f52e128c816e7b8
yngomeza <yngomeza@unjbg.edu.pe>
2025-06-01 18:56:00 -0500

    Agregar ejercicio5: Determinar si dos números son amigos

---
+++ ejercicio5_numeros_amigos.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int sumaDivisores(int n) {
+    int suma = 0;
+    for (int i = 1; i < n; i++) {
+        if (n % i == 0)
+            suma += i;
+    }
+    return suma;
+}
+
+int main() {
+    int a, b;
+    cout << "Ingresa dos numeros enteros positivos" << endl;
+    cout << " " << endl;
+    cout << "Primer numero: ";
+	cin >> a;
+	cout << "Segundo numero: ";
+	cin >> b;
+	cout << " " << endl;
+	
+    if (sumaDivisores(a) == b && sumaDivisores(b) == a)
+        cout << a << " y " << b << " son numeros amigos." << endl;
+    else
+        cout << a << " y " << b << " no son numeros amigos." << endl;
+
+    return 0;
+}
