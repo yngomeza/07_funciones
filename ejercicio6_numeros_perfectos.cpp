592e1bdd90c2c867260b46ad8e75f80ccf4b8af2
yngomeza <yngomeza@unjbg.edu.pe>
2025-06-02 23:06:20 -0500

    Agregar ejercicio6: Encontrar numeros perfectos

--- ejercicio6_numeros_perfectos.cpp
+++ ejercicio6_numeros_perfectos.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
 
-bool esPerfecto(int n) {
+int esPerfecto(int n) {
     int suma = 0;
     for (int i = 1; i < n; i++) {
         if (n % i == 0)
             suma += i;
     }
+    
     return suma == n;
 }
 
