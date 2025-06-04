347cdbc3f72cd507d96637920c4b5b51ab4b1870
Jonathan-md <jonathanpercamamani@gmail.com>
2025-06-04 13:44:07 -0500

    Agregar función MCD y MCM de dos numeros con referencias

---
+++ mcdymcm_r.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void MCDyMCM(int a, int b, int &mcd, int &mcm);
+
+int main(){
+	int x = 18, y = 24;
+	int rMCD, rMCM;
+	MCDyMCM(x, y, rMCD, rMCM);
+	cout << "MCD: " << rMCD << endl;
+	cout << "MCM: " << rMCM << endl;
+	
+	return 0;	
+}
+
+void MCDyMCM(int a, int b, int &mcd, int &mcm){
+	int i;
+	i = 1;
+	mcd = 1;
+	
+	while(i <= a && i <= b){
+		if(a % i == 0 && b % i == 0)
+			mcd = i;
+			i = i + 1;
+	}
+	mcm = (a * b)/mcd;
+}
